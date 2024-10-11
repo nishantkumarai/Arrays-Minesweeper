d60541e2bfa1c4dedd3d0478b1f370a3c27d1454
Nishant Kumar Rai <66172201+nishantkumarai@users.noreply.github.com>
2024-10-12 01:06:32 +0530

    update

--- Array-Minesweeper/header/UI/UIService.h
+++ Array-Minesweeper/header/UI/UIService.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../../header/UI/MainMenu/MainMenuUIController.h"
 #include "../../header/UI/SplashScreen/SplashScreenUIController.h"
+#include "../../header/UI/Gameplay/GameplayUIController.h"
 #include "../../header/UI/Credits/CreditsScreenUIController.h"
 #include "../../header/UI/Instructions/InstructionsScreenUIController.h"
 #include "../../header/UI/Interface/IUIController.h"
@@ -12,6 +13,7 @@ namespace UI
 	private:
 		SplashScreen::SplashScreenUIController* splash_screen_controller;
 		MainMenu::MainMenuUIController* main_menu_controller;
+		GameplayUI::GameplayUIController* gameplay_ui_controller;
 		Credits::CreditsScreenUIController* credit_screen_controller;
 		Instructions::InstructionsScreenUIController* instructions_screen_controller;
 
